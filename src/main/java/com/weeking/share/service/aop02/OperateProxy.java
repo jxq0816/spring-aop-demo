177c65b91f781cba5b5cb2e82cd5e98c1d1a1f19
jiangxingqi <xingqijiang@sohu-inc.com>
2018-02-09 10:02:55 +0800

    spring AOP 获取参数列表及方法

--- src/main/java/com/weeking/share/service/aop02/OperateProxy.java
+++ src/main/java/com/weeking/share/service/aop02/OperateProxy.java
@@ -1,10 +1,34 @@
 package com.weeking.share.service.aop02;
 
+import com.weeking.share.service.aop02.annotion.Operate;
+import com.weeking.share.service.aop02.ennum.OperateEnum;
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.reflect.MethodSignature;
+
+import java.lang.reflect.Method;
 
 public class OperateProxy{
 
 	public void doAfter(JoinPoint jp) throws NoSuchMethodException, SecurityException {
-		System.out.println("do after!");
+
+		MethodSignature signature = (MethodSignature) jp.getSignature();
+		Method method = signature.getMethod();
+		System.out.println("method : "+method.getName());
+
+		Method realMethod = jp.getTarget().getClass().getDeclaredMethod(signature.getName(), method.getParameterTypes());
+		System.out.println("realMethod : "+method.getName());
+
+		Operate operate = realMethod.getAnnotation(Operate.class);
+		Object[] args = jp.getArgs();
+		if( args.length > 0 && operate != null) {
+			OperateEnum status = operate.status();
+			System.out.println("keys:"+status);
+			System.out.print("values:");
+			for(int i=0;i<args.length;i++){
+				System.out.print(args[i]);
+			}
+		}
+		System.out.println();
+		System.out.println("do after end");
 	}
 }
